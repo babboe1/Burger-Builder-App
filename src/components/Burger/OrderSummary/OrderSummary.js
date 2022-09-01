bec8c379d2ddfbe5ad0a342881f6b944f540d14d
babboe1 <ayodelaustinab@gmail.com>
2022-09-01 22:52:33 +0100

    create Component for Order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Auxi from '../../../hoc/Auxi/Auxi';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+   componentDidUpdate() {
+      console.log('[Order Summary] component updated')
+   }
+	render() {
+		const ingredientSummary = Object.keys(this.props.data).map((key, idx) => {
+			return (
+				<li key={key+idx}>
+					<span style={{ textTransform: 'capitalize' }}>{key}</span>:
+					{this.props.data[key]}
+				</li>
+			);
+		});
+		return (
+			<Auxi>
+				<h3>Your Order</h3>
+				<p>A delicious Burger ith the following ingredients:</p>
+				<ul>{ingredientSummary}</ul>
+            <p>Continue to checkout?</p>
+            <p> <strong>Total Price: ${this.props.price }</strong></p>
+				<Button class="Danger" click={this.props.click}>
+					CANCEL
+				</Button>
+				<Button
+					class="Success"
+               click={() => {
+                  this.props.click()
+                  alert("continue")
+               }}
+				>
+					CONTINUE
+				</Button>
+			</Auxi>
+		);
+	}
+}
+
+export default OrderSummary;
