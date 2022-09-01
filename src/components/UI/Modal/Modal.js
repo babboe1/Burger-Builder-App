9c13e65db19690659c5713bd4d26061f6a337933
babboe1 <ayodelaustinab@gmail.com>
2022-09-01 22:55:08 +0100

    update modal; component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import classes from './Modal.css';
 
-const modal = (props) => {
-	return (
-		<div
-			className={classes.Modal}
-			style={{
-				transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-				opacity: props.show ? 1 : 0,
-				zIndex: props.show ? 500 : -1,
-				pointerEvents: props.show ? 'all' : 'none',
-			}}
-		>
-			{props.children}
-		</div>
-	);
+class Modal extends React.Component {
+   shouldComponentUpdate(nextProps, nextState) {
+      return nextProps.show !== this.props.show
+   }
+   componentDidUpdate() {
+      console.log('[Modal] component updated');
+   }
+
+	render() {
+		return (
+			<div
+				className={classes.Modal}
+				style={{
+					transform: this.props.show
+						? 'translateY(0)'
+						: 'translateY(-100vh)',
+					opacity: this.props.show ? 1 : 0,
+					zIndex: this.props.show ? 500 : -1,
+					pointerEvents: this.props.show ? 'all' : 'none',
+				}}
+			>
+				{this.props.children}
+			</div>
+		);
+	}
 };
 
-export default modal;
+export default Modal;
