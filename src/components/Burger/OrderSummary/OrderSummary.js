9002bb601523748e1a915cd02b6538550587bfc1
babboeCodes <babboecodes@gmail.com>
2022-09-15 12:14:20 +0100

    update order summary card

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,15 +1,17 @@
+// import axios from 'axios';
 import React, { Component } from 'react';
 import Auxi from '../../../hoc/Auxi/Auxi';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-   componentDidUpdate() {
-      console.log('[Order Summary] component updated')
-   }
+	componentDidUpdate() {
+		console.log('[Order Summary] component updated');
+	}
 	render() {
+	
 		const ingredientSummary = Object.keys(this.props.data).map((key, idx) => {
 			return (
-				<li key={key+idx}>
+				<li key={key + idx}>
 					<span style={{ textTransform: 'capitalize' }}>{key}</span>:
 					{this.props.data[key]}
 				</li>
@@ -20,18 +22,12 @@ class OrderSummary extends Component {
 				<h3>Your Order</h3>
 				<p>A delicious Burger ith the following ingredients:</p>
 				<ul>{ingredientSummary}</ul>
-            <p>Continue to checkout?</p>
+				<p>Continue to checkout?</p>
             <p> <strong>Total Price: ${this.props.price }</strong></p>
 				<Button class="Danger" click={this.props.click}>
 					CANCEL
 				</Button>
-				<Button
-					class="Success"
-               click={() => {
-                  this.props.click()
-                  alert("continue")
-               }}
-				>
+				<Button class="Success" click={this.props.purchase}>
 					CONTINUE
 				</Button>
 			</Auxi>
