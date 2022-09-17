334e66264e67e0c9636c32b14ccff6cb1c73b2c0
babboeCodes <babboecodes@gmail.com>
2022-09-17 13:24:36 +0100

    refactor: perform refactor and update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,95 +36,100 @@ class BurgerBuilder extends Component {
          })
    }
 
-	ingredientHandler = (e, operator) => {
-		const newState = { ...this.state.ingredients };
-		const fixedPrices = { ...this.state.fixedPrices };
-		const newPrices = { ...this.state.initialPrice };
-		let totalPrice = this.state.totalPrice;
+   ingredientHandler = (e, operator) => {
+      const newState = { ...this.state.ingredients };
+      const fixedPrices = { ...this.state.fixedPrices };
+      const newPrices = { ...this.state.initialPrice };
+      let totalPrice = this.state.totalPrice;
 
-		for (const key in newState) {
-			if (Object.hasOwnProperty.call(newState, key) && e.target.id === key) {
-				let element = null;
+      for (const key in newState) {
+         if (Object.hasOwnProperty.call(newState, key) && e.target.id === key) {
+            let element = null;
 
-				switch (operator) {
-					case '+':
-						element = newState[key] + 1;
-						newPrices[key] += fixedPrices[key];
-						totalPrice += fixedPrices[key];
-						break;
+            switch (operator) {
+               case '+':
+                  element = newState[key] + 1;
+                  newPrices[key] += fixedPrices[key];
+                  totalPrice += fixedPrices[key];
+                  break;
 
-					case '-':
-						element = newState[key] - 1 < 0 ? 0 : newState[key] - 1;
-						newPrices[key] =
-							newPrices[key] <= fixedPrices[key]
-								? 0
-								: newPrices[key] - fixedPrices[key];
-						totalPrice =
-							totalPrice <= 4 ? 4 : totalPrice - fixedPrices[key];
-						break;
+               case '-':
+                  element = newState[key] - 1 < 0 ? 0 : newState[key] - 1;
+                  newPrices[key] =
+                     newPrices[key] <= fixedPrices[key]
+                        ? 0
+                        : newPrices[key] - fixedPrices[key];
+                  totalPrice =
+                     totalPrice <= 4 ? 4 : totalPrice - fixedPrices[key];
+                  break;
 
-					default:
-						element = null;
-				}
-				newState[key] = element;
-			}
-		}
-		this.setState({
-			ingredients: newState,
-			initialPrice: newPrices,
-			totalPrice: +totalPrice.toFixed(1),
-		});
-	};
+               default:
+                  element = null;
+            }
+            newState[key] = element;
+         }
+      }
+      this.setState({
+         ingredients: newState,
+         initialPrice: newPrices,
+         totalPrice: +totalPrice.toFixed(1),
+      });
+   };
 
-	test = () => {
-		this.setState({ loading: true });
-	};
-	onOrderClick = () => {
-		if (!this.state.showModal) {
-			document.body.classList.add('StopScroll');
-		} else document.body.classList.remove('StopScroll');
-		this.setState({
-			showModal: !this.state.showModal,
-		});
-	};
+   onOrderClick = () => {
+      if (!this.state.showModal) {
+         document.body.classList.add('StopScroll');
+      } else document.body.classList.remove('StopScroll');
+      this.setState({
+         showModal: !this.state.showModal,
+         credits: false,
+      });
+   };
 
-	onOrderPurchase = () => {
-		document.body.classList.add('StopScroll');
-		this.setState({
-			loading: true,
-		});
-		const order = {
-			ingredients: this.props.ingredients,
-			price: this.props.price,
-			customer: {
-				name: 'Max',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '41351',
-					country: 'Germany',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		};
-		axios
-			.post(`/orders.json`, order)
-			.then((res) => {
-				console.log(res);
-				this.setState({
-					loading: false,
-					showModal: !this.state.showModal,
-				});
-				document.body.classList.remove('StopScroll');
-			})
-			.catch((err) =>
-				this.setState({
-					error: !this.state.error,
-					loading: false,
-				}),
-			);
-	};
-	render() {
+   onOrderPurchase = () => {
+      document.body.classList.add('StopScroll');
+      this.setState({
+         loading: true,
+      });
+      const order = {
+         ingredients: this.props.ingredients,
+         price: this.props.price,
+         customer: {
+            name: 'Max',
+            address: {
+               street: 'Teststreet 1',
+               zipCode: '41351',
+               country: 'Germany',
+            },
+            email: 'test@test.com',
+         },
+         deliveryMethod: 'fastest',
+      };
+      axios
+         .post(`/orders`, order)
+         .then((res) => {
+            this.setState({
+               // showModal: !this.state.showModal,
+               loading: false,
+               credits: true,
+            });
+            // console.log(res);
+            // document.body.classList.remove('StopScroll');
+         })
+         .catch((err) => {
+            this.setState({
+               error: true,
+               loading: false,
+               credits: true
+            });
+            // console.log(err, this.state.error, this.state.credits);
+         });
+   };
+   render() {
+      // console.log(
+      //    axios.get('/.json').then((res) => console.log(res.data.ingredients)),
+      // );
+
       const newObject = { ...this.state.ingredients };
       for (const key in newObject) {
          if (Object.hasOwnProperty.call(newObject, key)) {
@@ -154,35 +159,47 @@ class BurgerBuilder extends Component {
             </p>
          );
       }
+         
 
-		const showBackdrop = this.state.showModal ? (
-			<Backdrop click={this.onOrderClick} />
-		) : null;
+      const showBackdrop = this.state.showModal ? (
+         <Backdrop click={this.onOrderClick} />
+      ) : null;
 
-		return (
-			<AuthContext.Provider
-				value={{
-					addIngredient: (e) => this.ingredientHandler(e, '+'),
-					removeIngredient: (e) => this.ingredientHandler(e, '-'),
-				}}
-			>
-				<Auxi>
-					{showBackdrop}
-					<Modal show={this.state.showModal} load={this.state.loading}>
-						{orderSummary}
-					</Modal>
-					<Burger ingredients={this.state.ingredients} />
-					<BuildControls
-						object={this.state.ingredients}
-						price={this.state.totalPrice}
-						disabled={newObject}
-						orderDisabled={this.state.ingredients}
-						click={this.onOrderClick}
-					/>
-				</Auxi>
-			</AuthContext.Provider>
-		);
-	}
+      return (
+         <AuthContext.Provider
+            value={{
+               addIngredient: (e) => this.ingredientHandler(e, '+'),
+               removeIngredient: (e) => this.ingredientHandler(e, '-'),
+            }}
+         >
+            <Auxi>
+               {showBackdrop}
+               <Modal show={this.state.showModal} load={this.state.loading}>
+                  {orderSummary}
+               </Modal>
+               {!this.state.ingredients ? (
+                  <Spinner />
+               ) : (
+                  <Auxi>
+                     <Burger ingredients={this.state.ingredients} />
+                     <BuildControls
+                        object={this.state.ingredients}
+                        price={this.state.totalPrice}
+                        disabled={newObject}
+                        orderDisabled={this.state.ingredients}
+                        click={this.onOrderClick}
+                     />
+                  </Auxi>
+               )}
+               {!this.state.ingredients ? (
+                  <p style={{ textAlign: 'center' }}>
+                     <strong>Error!!, can't load ingredients</strong>
+                  </p>
+               ) : null}
+            </Auxi>
+         </AuthContext.Provider>
+      );
+   }
 }
 
 export default withErrorHandler(BurgerBuilder, axios);
